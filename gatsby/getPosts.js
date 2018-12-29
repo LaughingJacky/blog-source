0da6d7521307bf52f138b332601a0cf5dac12a1b
ShawbWong <laughingjacky.vi@gmail.com>
2018-12-29 14:14:42 +0800

    update

--- gatsby/getPosts.js
+++ gatsby/getPosts.js
@@ -1,10 +1,14 @@
 const axios = require('axios')
 
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+// require('dotenv').config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// })
 
-const { API_BASE_URL, API_SPACE_ID, API_TOKEN } = process.env
+const API_BASE_URL = 'https://cdn.contentful.com'
+const API_SPACE_ID = '49fdbwmpdowy'
+const API_TOKEN = '63caeb300ae46255595611c480207a0e43d1fd688af238a1020b8ec0985ff826'
+
+// const { API_BASE_URL, API_SPACE_ID, API_TOKEN } = process.env
 
 const getPosts = async (contentType) => {
   const POST_URL = `${API_BASE_URL}/spaces/${API_SPACE_ID}/entries`
