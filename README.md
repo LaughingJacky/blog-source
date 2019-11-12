6432c949105fce2d1d9a153a3739d96c9c4acc77
ShawbWong <laughingjacky.vi@gmail.com>
2019-11-12 11:34:38 +0800

    update offline plugin

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Hosted on Netlify & Now.
 
 ## 主题: forty
 
-TODO: Service Worker support
+~~TODO: Service Worker support~~
 
 [HTML5 UP](https://html5up.net/forty)
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,11 @@
 import 'prismjs/themes/prism-solarizedlight.css';
 
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+        `This application has been updated. ` +
+        `Reload to display the latest version?`
+    )
+    if (answer === true) {
+        window.location.reload()
+    }
+}
