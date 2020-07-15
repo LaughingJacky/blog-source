ff4d1a541d874887370a22a156ec129eca05714b
ShawbWong <laughingjacky.vi@gmail.com>
2020-07-15 11:39:06 +0800

    update deps

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,11 @@ exports.onCreateNode = require('./gatsby/onCreateNode')
 exports.createPages = require('./gatsby/CreatePages')
 
 exports.onCreateWebpackConfig = require('./gatsby/onCreateWebpackConfig')
+
+exports.createSchemaCustomization = ({ actions }) => {
+    actions.createTypes(`
+      type SitePage implements Node @dontInfer {
+        path: String!
+      }
+    `)
+}
